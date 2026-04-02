627afaa2e77cede66e19799a2c9fe5efa159f8e6
barrotsteindev <barrotstein@gmail.com>
2026-04-02 15:50:15 +0300

    test: sync test infrastructure with upstream starter-kit
    Run tests inside cockpit-project/tasks container via podman.
    This provides all required Python deps (aiohttp, webdriver_bidi).
    Fixes 'ModuleNotFoundError: No module named aiohttp' test failure.

--- test/browser/run-test.sh
+++ test/browser/run-test.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 set -eux
 
+cd "${SOURCE}"
+
 # tests need cockpit's bots/ libraries and test infrastructure
-cd $SOURCE
 git init
 rm -f bots  # common local case: existing bots symlink
 make bots test/common
 
-# support running from clean git tree
-if [ ! -d node_modules/chrome-remote-interface ]; then
-    # copy package.json temporarily otherwise npm might try to install the dependencies from it
-    rm -f package-lock.json  # otherwise the command below installs *everything*, argh
-    mv package.json .package.json
-    # only install a subset to save time/space
-    npm install chrome-remote-interface
-    mv .package.json package.json
-fi
-
 # disable detection of affected tests; testing takes too long as there is no parallelization
 mv .git dot-git
 
-. /etc/os-release
+. /run/host/usr/lib/os-release
 export TEST_OS="${ID}-${VERSION_ID/./-}"
 
-if [ "${TEST_OS#centos-}" != "$TEST_OS" ]; then
+if [ "$TEST_OS" = "centos-9" ]; then
     TEST_OS="${TEST_OS}-stream"
 fi
+
+# Chromium sometimes gets OOM killed on testing farm
+export TEST_BROWSER=firefox
 
 EXCLUDES=""
 
@@ -33,10 +27,15 @@ EXCLUDES=""
 echo "TEST_ALLOW_JOURNAL_MESSAGES: ${TEST_ALLOW_JOURNAL_MESSAGES:-}"
 echo "TEST_AUDIT_NO_SELINUX: ${TEST_AUDIT_NO_SELINUX:-}"
 
+GATEWAY="$(python3 -c 'import socket; print(socket.gethostbyname("_gateway"))')"
 RC=0
-test/common/run-tests --nondestructive --machine 127.0.0.1:22 --browser 127.0.0.1:9090 $EXCLUDES || RC=$?
+./test/common/run-tests \
+    --nondestructive \
+    --machine "${GATEWAY}":22 \
+    --browser "${GATEWAY}":9090 \
+    $EXCLUDES \
+|| RC=$?
 
 echo $RC > "$LOGS/exitcode"
 cp --verbose Test* "$LOGS" || true
-# deliver test result via exitcode file
-exit 0
+exit $RC
